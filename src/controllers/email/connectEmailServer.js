49e4ea83b1c90e9bb168689b28e5ead2fdb7d06a
anuprajwal <anuprajwal89@gmail.com>
2026-09-25 00:34:35 +0530

    auebcdn

--- src/controllers/email/connectEmailServer.js
+++ src/controllers/email/connectEmailServer.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // true for port 465, false for others
+  secure: process.env.SMTP_port === '465',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-  pool: true,               // keep the connection alive
-  maxConnections: 5,        // adjust to taste
+  pool: true,
+  maxConnections: 5,
   maxMessages: 100,
   connectionTimeout: 40000,
   greetingTimeout: 40000,
